38a02f7ac7d409d6c94bcb2bad1c1426b414ee35
Wilfredo Alcala <alcalawilfre@gmail.com>
2018-12-02 17:01:51 -0300

    Modified to read por from .env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('./app');
+const PORT = process.env.PORT || 3000
+
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log('Server listening at port: ', 3000);
+server.listen(PORT, () => {
+    console.log('Server listening at port: ', PORT);
 });
