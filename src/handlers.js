4bbd5797e0711f789b4a949ce6ca315a00611ebc
Wilfredo Alcala <alcalawilfre@gmail.com>
2018-12-03 23:32:10 -0300

    Delete mock files

--- src/handlers.js
+++ src/handlers.js
@@ -1,10 +1,10 @@
-const utils = require('./utils/utils');
-const led = utils.getLedInstance(17);
+const Gpio = require('onoff');
+const led = new Gpio(17, 'out');
 
 const setLed = (value) => {
     return new Promise((resolve, reject) => {
-        led.write(value, (err) =>{
-            if(err) {
+        led.write(value, (err) => {
+            if (err) {
                 return reject(err);
             }
             resolve('Success!');
@@ -13,5 +13,5 @@ const setLed = (value) => {
 }
 
 module.exports = {
-    setLed    
+    setLed
 }
--- src/utils/onoffSelector.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-let gpioSupport = (process.env['USE_GPIO'] || 'FALSE').toLocaleUpperCase() === 'TRUE';
-
-module.exports = gpioSupport
-    ? require('onoff')
-    : require('./onoff-mock');
