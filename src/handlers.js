ccb253a4a3993735063f1b2eb3dd8243cd09351d
Wilfredo Alcala <alcalawilfre@gmail.com>
2018-12-04 23:54:41 -0300

    Modify setLed to write sync

--- src/handlers.js
+++ src/handlers.js
@@ -2,14 +2,7 @@ const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 
 const setLed = (value) => {
-    return new Promise((resolve, reject) => {
-        led.write(value, (err) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve('Success!');
-        });
-    });
+        led.writeSync(value);
 }
 
 module.exports = {
